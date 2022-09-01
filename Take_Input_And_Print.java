614f79416e383eeac4b4f2aab0c34f20ffaff32c
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-09-01 11:02:48 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String y= sc.nextLine();         System.out.print(y);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String y= sc.nextLine();
+        System.out.print(y);
+    }
+}
