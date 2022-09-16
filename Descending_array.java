7bb68915f1df4f75570405af4d2d6dd27f89c69a
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-09-16 11:01:46 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],c=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int i=0;i<(n-1);i++)         {             if(x[i]>x[i+1])             {                 c+=1;             }         }         if(c==(n-1))         {             System.out.println("yes");         }         else         {             System.out.println("no");         }         sc.close();
        }
    }

---
+++ Descending_array.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int i=0;i<(n-1);i++)
+        {
+            if(x[i]>x[i+1])
+            {
+                c+=1;
+            }
+        }
+        if(c==(n-1))
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+        sc.close();
+        
+    }
+}
