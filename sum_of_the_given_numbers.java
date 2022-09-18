85f424b7ba8cc8048e33ff262c24bb72ac0e03d8
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-09-18 23:24:03 +0530

    import java.util.Scanner; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         for(int i=0;i<a;i++)         {             int c,b;             c=sc.nextInt();             b=sc.nextInt();              System.out.println(c+b);         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        for(int i=0;i<a;i++)
+        {
+            int c,b;
+            c=sc.nextInt();
+            b=sc.nextInt();
+             System.out.println(c+b);
+        }
+    }
+}
