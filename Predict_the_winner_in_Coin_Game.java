64505c8be6b35ba45ca51139e029191405348f40
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-09-18 21:51:54 +0530

    import java.util.Scanner; class winner {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=sc.nextInt();         if(a%2==0||b%2==0)         System.out.print("Player 1");         else         System.out.print("Player 2");     } }

---
+++ Predict_the_winner_in_Coin_Game.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class winner
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        if(a%2==0||b%2==0)
+        System.out.print("Player 1");
+        else
+        System.out.print("Player 2");
+    }
+}
