b8adebc3a9c66ebbb0e3dc5eb99e34b6426ef91b
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-09-23 09:55:34 +0530

    import java.util.Scanner; class smaple {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++)         {             for(int j=0;j<n;j++)             {                 if(i==j)                 {                     System.out.print("0");                 }                 else                 {
                        System.out.print("x");
                    }
                }
                System.out.print("
    ");
            }
        }
    }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class smaple
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j)
+                {
+                    System.out.print("0");
+                }
+                else
+                {
+                    
+                    System.out.print("x");
+                }
+            }
+            System.out.print("
+");
+        }
+    }
+}
