cf6089a02f5670ba321628d5ed83e09b936758ac
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-10-13 15:54:08 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a[],c=0;         n=sc.nextInt();         a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=1;i<n-1;i++)         {                 if((a[i-1]%2==0 && a[i+1]%2!=0) || (a[i-1]%2!=0 && a[i+1]%2==0))                 {                     c+=1;                 }         }         System.out.println(c);
        }
    }

---
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a[],c=0;
+        n=sc.nextInt();
+        a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=1;i<n-1;i++)
+        {
+                if((a[i-1]%2==0 && a[i+1]%2!=0) || (a[i-1]%2!=0 && a[i+1]%2==0))
+                {
+                    c+=1;
+                }
+        }
+        System.out.println(c);
+        
+    }
+}
