15d8b0e3896a010534d3233b8772bc27efc0ccde
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-10-15 13:58:22 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,m,c=0,s=0;         n=sc.nextInt();         m=sc.nextInt();         ArrayList<Integer> a=new ArrayList<Integer>();         ArrayList<Integer> b=new ArrayList<Integer>();         for(int i=0;i<n;i++)         {             a.add(sc.nextInt());         }         for(int i=0;i<m;i++)         {             b.add(sc.nextInt());         }         HashSet<Integer> hs1=new HashSet<Integer>(a);         HashSet<Integer> hs2=new HashSet<Integer>(b);         for(int i:hs1)         {             for(int j:hs2)             {                 if(i==j)                 {                     c++;                 }             }         }         hs1.addAll(hs2);         s=hs1.size();         System.out.println(s-c);     } }

---
+++ Count_elements_II.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,m,c=0,s=0;
+        n=sc.nextInt();
+        m=sc.nextInt();
+        ArrayList<Integer> a=new ArrayList<Integer>();
+        ArrayList<Integer> b=new ArrayList<Integer>();
+        for(int i=0;i<n;i++)
+        {
+            a.add(sc.nextInt());
+        }
+        for(int i=0;i<m;i++)
+        {
+            b.add(sc.nextInt());
+        }
+        HashSet<Integer> hs1=new HashSet<Integer>(a);
+        HashSet<Integer> hs2=new HashSet<Integer>(b);
+        for(int i:hs1)
+        {
+            for(int j:hs2)
+            {
+                if(i==j)
+                {
+                    c++;
+                }
+            }
+        }
+        hs1.addAll(hs2);
+        s=hs1.size();
+        System.out.println(s-c);
+    }
+}
